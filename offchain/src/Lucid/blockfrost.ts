38a68b9affbe42c7e1189d4cc22c46666ae403f8
rhystmorgan <morgan.rhystomas@gmail.com>
2025-02-10 17:49:05 +0000

    tidy up scripts

--- offchain/src/Lucid/blockfrost.ts
+++ offchain/src/Lucid/blockfrost.ts
@@ -4,7 +4,7 @@ export async function blockfrost() {
   const lucid = await Lucid(
     new Blockfrost(
       "https://cardano-preprod.blockfrost.io/api/v0",
-      'preprodx670GWOQ6OC85fETu6xd0Gbu7nZuZI6W',
+      '',
     ),
     "Preprod",
   );
